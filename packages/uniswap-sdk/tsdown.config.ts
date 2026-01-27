ed9957c6e15fa733d4db75195c58c90bf5f9ce8a
Akshat Mittal <itsreallyakshat@gmail.com>
2026-01-27 19:44:33 +0530

    Cleanup

--- packages/uniswap-sdk/tsdown.config.ts
+++ packages/uniswap-sdk/tsdown.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
-  entry: ["./src/index.tsx"],
-  external: ["react"],
-  dts: true,
+  entry: ["./src/index.ts"],
+  external: ["viem"],
   format: ["esm"],
+  dts: true,
 });
