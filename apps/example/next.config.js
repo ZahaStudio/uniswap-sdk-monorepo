a7cb16d294ee511f4828bda6789525d63787318b
Sneh Koul <35871990+Sneh1999@users.noreply.github.com>
2026-02-13 18:11:39 +0530

    fix: integration tests (#12)

--- apps/example/next.config.js
+++ apps/example/next.config.js
@@ -1,8 +1,20 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  transpilePackages: [
-    "@zahastudio/uniswap-sdk",
-    "@zahastudio/uniswap-sdk-react",
-  ],
+  turbopack: {
+    resolveAlias: {
+      wagmi: "@zahastudio/uniswap-sdk-react/node_modules/wagmi",
+      "wagmi/query": "@zahastudio/uniswap-sdk-react/node_modules/wagmi/query",
+    },
+  },
+  transpilePackages: ["@zahastudio/uniswap-sdk", "@zahastudio/uniswap-sdk-react"],
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      wagmi: require.resolve("@zahastudio/uniswap-sdk-react/node_modules/wagmi"),
+      "wagmi/query": require.resolve("@zahastudio/uniswap-sdk-react/node_modules/wagmi/query"),
+    };
+
+    return config;
+  },
 };
--- packages/uniswap-sdk/test/fixtures/unichain.ts
+++ packages/uniswap-sdk/test/fixtures/unichain.ts
@@ -1,18 +1,18 @@
 import { sortTokens } from "@/helpers/tokens";
 import { FeeTier } from "@/utils/getPool";
 
-export const MAINNET_TOKENS = {
+export const UNICHAIN_TOKENS = {
   ETH: "0x0000000000000000000000000000000000000000",
   USDC: "0x078d782b760474a361dda0af3839290b0ef57ad6",
 } as const;
 
-export const MAINNET_POOL_ID = "0x3258f413c7a88cda2fa8709a589d221a80f6574f63df5a5b6774485d8acc39d9" as const;
+export const UNICHAIN_POOL_ID = "0x3258f413c7a88cda2fa8709a589d221a80f6574f63df5a5b6774485d8acc39d9" as const;
 
-export const MAINNET_FORK_BLOCK_NUMBER = 39_629_268;
+export const UNICHAIN_FORK_BLOCK_NUMBER = 39_629_268;
 
-const [currency0, currency1] = sortTokens(MAINNET_TOKENS.ETH, MAINNET_TOKENS.USDC);
+const [currency0, currency1] = sortTokens(UNICHAIN_TOKENS.ETH, UNICHAIN_TOKENS.USDC);
 
-export const MAINNET_POOL_KEY = {
+export const UNICHAIN_POOL_KEY = {
   currency0,
   currency1,
   fee: FeeTier.LOW,
